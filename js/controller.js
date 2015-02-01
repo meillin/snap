b617779a51b37326de0b45e54459076490de5785
Meilan Lin <meilan.dk@gmail.com>
2015-02-01 22:35:59 +0100

    Add Angular route and controller

---
+++ js/controller.js
@@ -0,0 +1,9 @@
+var vc = angular.module('viewControllers', []);
+
+vc.controller("homeCtrl", function($scope) {
+	console.log('homecontrl.js')
+});
+
+vc.controller("basicCtrl", function($scope) {
+	console.log('basicCtrl.js')
+});
--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-var app = angular.module("myApp", []);
+var app = angular.module('myApp', ['ngRoute', 'viewControllers']);
+
+app.config(['$routeProvider',
+	function ($routeProvider) {
+		$routeProvider.
+			when('/home', {
+				templateUrl: 'temp/home.html',
+				controller: 'homeCtrl'
+			}).
+			when('/basic', {
+				templateUrl: 'temp/basic.html',
+				controller: 'basicCtrl'
+			}).
+			otherwise({
+				redirectTo: '/home'
+			});
+}]);
